3088132bfdbb8727e4c60236eeeaa81808213de9
João Victor do Nascimento <nascimentojoaovictor21@gmail.com>
2023-01-05 21:42:28 -0300

    first cart layout #3

--- layout/card-product.tsx
+++ layout/card-product.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import { Product } from '../interface/Product';
-import Link from './link';
+import React from 'react'
+import { Product } from '../interface/Product'
+import Link from './link'
 
-type Props = {
-    className?: string;
-    product?: Product;
+interface Props {
+  className: string
+  product: Product
 }
 
-export default function CardProduct(props: Props) {
-    const product = props.product || new Product(
-        'Produto 1',
-        1.50,
-        'Texto',
-        'produto-1',
-        [
-            'https://images.pexels.com/photos/5946087/pexels-photo-5946087.jpeg',
-            'https://images.pexels.com/photos/4032973/pexels-photo-4032973.jpeg'
-        ]
-    )
+export default function CardProduct (props: Props): JSX.Element {
+  const product = props.product
   return (
     <div className={`card ${props.className}`}>
         <img src={product.images[0]} className="card-img-top" alt="..."/>
