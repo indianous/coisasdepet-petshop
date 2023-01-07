5faa895e50c34ab16319cd4b314f4e23010ab779
João Victor do Nascimento <36607540+indianous@users.noreply.github.com>
2023-01-06 22:27:08 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 import Script from '../layout/script'
