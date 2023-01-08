3dea9bc9cf9cd7333e24e14d331845126b57d073
João Victor do Nascimento <36607540+indianous@users.noreply.github.com>
2023-01-07 21:13:26 -0300

    Update card-product.tsx

--- layout/card-product.tsx
+++ layout/card-product.tsx
@@ -11,21 +11,21 @@ export default function CardProduct (props: Props): JSX.Element {
   const product = props.product
   return (
     <div class="card">
-      <div class="row g-0 p-2">
-        <div class="col-4 p-3">
-          <img src={product.images[0]} alt="..." class="img-fluid">
+      <div className="row g-0 p-2">
+        <div className="col-4 p-3">
+          <img src={product.images[0]} alt="..." className="img-fluid">
         </div>
-        <div class="col-8 ps-2 d-flex flex-column justify-content-around">
-          <div class="border-bottom">
+        <div className="col-8 ps-2 d-flex flex-column justify-content-around">
+          <div className="border-bottom">
             <h3>{product.name}</h3>
           </div>
-          <div class="display-5">
+          <div className="display-5">
             Por: R&#36;{product.price}
           </div>
         </div>
-        <div class="d-flex flex-column btn-group-vertical">
+        <div className="d-flex flex-column btn-group-vertical">
           <Link href={`product/${product.slug}`} className="btn bg-primary">Comprar</Link>
-          <button class="btn bg-success">Adicionar ao carrinho</button>
+          <button className="btn bg-success">Adicionar ao carrinho</button>
         </div>
       </div>
   </div>
