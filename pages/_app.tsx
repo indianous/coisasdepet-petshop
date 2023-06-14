8adea8976793a6887519a54740e60df7b1585196
João Victor do Nascimento <nascimentojoaovictor21@gmail.com>
2023-06-14 17:00:23 -0300

    new aplication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.css'
-import Script from '../layout/script'
 
 export default function App ({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></Script>
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></Script>
       <Component {...pageProps} />
     </>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
-import Header from '../layout/header'
-import Footer from '../layout/footer'
 
 export default function Document (): JSX.Element {
   return (
     <Html lang="pt-br">
       <Head />
       <body>
-        <Header/>
         <Main />
         <NextScript />
-        <Footer/>
       </body>
     </Html>
   )
