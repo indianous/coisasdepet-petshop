00e05548c47d2c646d390481d0c4bc28994caf66
João Victor do Nascimento <nascimentojoaovictor21@gmail.com>
2023-06-17 17:15:47 -0300

    add gtag

---
+++ lib/gtag.ts
@@ -0,0 +1,1 @@
+export const GA_TRACKING_ID = "G-MZQW6H9JHE";
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
-export default function Document (): JSX.Element {
+export default function Document(): JSX.Element {
   return (
     <Html lang="pt-br">
       <Head />
       <body>
+        <Script
+          src={"https://www.googletagmanager.com/gtag/js?id=" + GA_TRACKING_ID}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
